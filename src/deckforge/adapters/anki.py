af4383ff74974f4d969862b34facaf8146515f2f
peckerw00d <balkarov05@gmail.com>
2026-02-16 19:52:07 +0300

    added anki adapter

---
+++ src/deckforge/adapters/anki.py
@@ -0,0 +1,30 @@
+import random
+
+from genanki import Deck, Model, Note, Package
+
+
+class AnkiAdapter:
+    def __init__(self):
+        self.basic_model = Model(
+            model_id=random.randrange(1 << 30, 1 << 31),
+            name="Basic",
+            fields=[{"name": "Sentence"}, {"name": "Translation"}],
+            templates=[
+                {
+                    "name": "Card 1",
+                    "qfmt": "{{Sentence}}",
+                    "afmt": '{{FrontSide}}<hr id="answer">{{Translation}}',
+                }
+            ],
+        )
+        self.deck = Deck(
+            deck_id=random.randrange(1 << 30, 1 << 31),
+            name=str(random.randrange(1 << 30, 1 << 31)),
+        )
+
+    def add_card(self, sentence: str, translation: str):
+        note = Note(model=self.basic_model, fields=[sentence, translation])
+        self.deck.add_note(note)
+
+    def export_deck(self):
+        Package(self.deck).write_to_file(f"media/{self.deck.deck_id}.apkg")
