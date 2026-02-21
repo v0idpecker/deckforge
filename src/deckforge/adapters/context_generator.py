919cea5559325231437809e77b5d2607f56dcbd5
peckerw00d <balkarov05@gmail.com>
2026-02-21 14:45:52 +0300

    fixed parallel corpus injection in context generator

--- src/deckforge/adapters/context_generator.py
+++ src/deckforge/adapters/context_generator.py
@@ -5,12 +5,12 @@ from tatoebatools import ParallelCorpus
 
 class ContextGenerator:
     def __init__(self, corpus: ParallelCorpus):
-        self._corpus = ParallelCorpus
+        self._corpus = corpus
 
     def get_context_sentence(self, word: str, limit: int) -> List[dict]:
         examples = []
 
-        for sentence, translation in self._corpus("eng", "rus"):
+        for sentence, translation in self._corpus:
             if word.lower() in sentence.text.lower():
                 examples.append({"english": sentence.text, "russian": translation.text})
 
