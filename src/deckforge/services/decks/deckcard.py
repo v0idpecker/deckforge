7c279e844e350e6a071c01fc6b1a93dd9c5a0af4
inal <balkarov05@gmail.com>
2026-08-09 11:13:53 +0300

    added deck card service

---
+++ src/deckforge/services/decks/deckcard.py
@@ -0,0 +1,31 @@
+from typing import List
+from uuid import UUID
+
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from deckforge.db.dao.decks import DeckCardDAO
+from deckforge.db.errors import DAOError
+from deckforge.dto.deck_card import DeckCardDTO
+from deckforge.services.errors import ServiceError
+
+
+class DeckCardService:
+    def __init__(
+        self, deckcard_dao: DeckCardDAO, sessionmaker: async_sessionmaker[AsyncSession]
+    ):
+        self._deckcard_dao = deckcard_dao
+        self._sessionmaker = sessionmaker
+
+    async def replace_for_item(self, item_id: UUID, cards: List[DeckCardDTO]):
+        try:
+            async with self._sessionmaker() as session, session.begin():
+                await self._deckcard_dao.replace_for_item(session, item_id, cards)
+        except DAOError as e:
+            raise ServiceError(str(e)) from e
+
+    async def list_by_task(self, task_id: UUID) -> List[DeckCardDTO]:
+        try:
+            async with self._sessionmaker() as session, session.begin():
+                return await self._deckcard_dao.list_by_task(session, task_id)
+        except DAOError as e:
+            raise ServiceError(str(e)) from e
